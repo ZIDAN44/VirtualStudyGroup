61ddfeae3bcccd8378f6d13fa03276d056b934dd
Zinadin Zidan <zidan.roking@gmail.com>
2024-12-11 03:06:18 +0600

    VSG: Show more info before joining
    * -----------------------------------------------
      | group_name (group_handle)                        [Join Group Button]
      | description
      -----------------------------------------------
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>

--- join_group.php
+++ join_group.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user_id'])) {
 $user_id = $_SESSION['user_id'];
 
 // Fetch groups the user is NOT a member of
-$stmt = $conn->prepare("SELECT g.group_id, g.group_name, g.description
+$stmt = $conn->prepare("SELECT g.group_id, g.group_name, g.group_handle, g.description
                         FROM groups g
                         LEFT JOIN group_members gm ON g.group_id = gm.group_id AND gm.user_id = ?
                         WHERE gm.group_id IS NULL");
@@ -28,6 +28,7 @@ $available_groups = $stmt->get_result();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Join a Group - Virtual Study Group</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/group_style.css">
 </head>
 <body>
     <?php include 'includes/header.php'; ?>
@@ -46,14 +47,19 @@ $available_groups = $stmt->get_result();
     ?>
 
     <?php if ($available_groups->num_rows > 0): ?>
-        <ul>
+        <ul class="group-list">
             <?php while ($group = $available_groups->fetch_assoc()): ?>
-                <li>
-                    <h4><?php echo htmlspecialchars($group['group_name']); ?></h4>
-                    <p><?php echo htmlspecialchars($group['description']); ?></p>
-                    <form action="process_join_group.php" method="POST">
+                <li class="group-item">
+                    <div class="group-info">
+                        <h4>
+                            <?php echo htmlspecialchars($group['group_name']); ?> 
+                            <span class="group-handle">(<?php echo htmlspecialchars($group['group_handle']); ?>)</span>
+                        </h4>
+                        <p><?php echo htmlspecialchars($group['description']); ?></p>
+                    </div>
+                    <form action="process_join_group.php" method="POST" class="join-group-form">
                         <input type="hidden" name="group_id" value="<?php echo $group['group_id']; ?>">
-                        <button type="submit">Join Group</button>
+                        <button type="submit" class="join-group-button">Join Group</button>
                     </form>
                 </li>
             <?php endwhile; ?>
