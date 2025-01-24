b41581916472ffc68168b1448c72ed029097f112
Zinadin Zidan <zidan.roking@gmail.com>
2025-01-24 11:47:10 +0600

    VSG: Fix and Re-design header/footer
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,23 @@
 <?php
 ?>
 <header>
+    <link rel="stylesheet" href="css/header.css">
     <nav>
-        <a href="<?= isset($_SESSION['user_id']) ? 'dashboard.php' : 'index.php' ?>">Home</a> |
-        <?php if (isset($_SESSION['user_id'])): ?>
-            <a href="dashboard.php">Dashboard</a> |
-            <a href="logout.php">Logout</a>
-        <?php else: ?>
-            <a href="login.php">Login</a> |
-            <a href="register.php">Register</a>
-        <?php endif; ?>
+        <!-- Left Links: Home -->
+        <div class="left-links">
+            <a href="<?= isset($_SESSION['user_id']) ? 'dashboard.php' : 'index.php' ?>">Home</a>
+        </div>
+
+        <!-- Right Links: Welcome Message and Profile -->
+        <div class="right-links">
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <a href="user_profile.php?user_id=<?= $_SESSION['user_id'] ?>" class="welcome-message">
+                    Welcome, <?= htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8') ?>
+                </a>
+            <?php elseif (basename($_SERVER['PHP_SELF']) !== 'register.php' && basename($_SERVER['PHP_SELF']) !== 'login.php'): ?>
+                <a href="login.php">Login</a> |
+                <a href="register.php">Register</a>
+            <?php endif; ?>
+        </div>
     </nav>
 </header>
