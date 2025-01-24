8fab0e69c8f71e713b512c97d58aa0e8965ca6ff
Zinadin Zidan <zidan.roking@gmail.com>
2025-01-24 12:11:32 +0600

    VSG: Logout to `Home Page`
    * Redirect to the index page
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>

--- logout.php
+++ logout.php
@@ -24,6 +24,6 @@ if (session_status() === PHP_SESSION_ACTIVE) {
     session_destroy();
 }
 
-// Redirect to the login page
-header("Location: login.php");
+// Redirect to the Home page
+header("Location: index.php");
 exit();
